50c5864bff8b1b0e36771df74f7c9db910764076
danilaKazimir <kazimird99@gmail.com>
2024-09-15 02:43:24 +0400

    delete locators.py

--- pages/locators.py
+++
@@ -1,8 +0,0 @@
-class InventoryPageLocators:
-    TWITTER_LINK_DATA_TEST_VALUE = "social-twitter"
-    FACEBOOK_LINK_DATA_TEST_VALUE = "social-facebook"
-    LINKEDIN_LINK_DATA_TEST_VALUE = "social-linkedin"
-    FOOTER_COPYRIGHT_TEXT_DATA_TEST_VALUE = "footer-copy"
-    BURGER_BUTTON = "#react-burger-menu-btn"
-    SAUCE_LABS_COM_LINK_VALUE = "About"
-    LOGOUT_LINK_VALUE = "Logout"
